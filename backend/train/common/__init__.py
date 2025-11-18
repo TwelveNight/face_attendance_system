4ce231169b934f1300c74f8a537334d1d9dd57f1
twelvenight <1315640046@qq.com>
2025-11-18 16:30:32 +0800

    chore:更新共享工具模块，添加load_dataset_by_class到导入和__all__列表

--- backend/train/common/__init__.py
+++ backend/train/common/__init__.py
@@ -1,10 +1,11 @@
 """训练模块共享工具"""
 from .yolo_detector import YOLOFaceDetector
-from .data_utils import ensure_dir, save_face_image, load_face_images
+from .data_utils import ensure_dir, save_face_image, load_face_images, load_dataset_by_class
 
 __all__ = [
     'YOLOFaceDetector',
     'ensure_dir',
     'save_face_image', 
-    'load_face_images'
+    'load_face_images',
+    'load_dataset_by_class'
 ]
